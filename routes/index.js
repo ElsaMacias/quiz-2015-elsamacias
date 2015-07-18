1a62fe4e2632f9a364ea31b0421984072ebd1ca5
ElsaMacias <elsa.macias@gmail.com>
2015-07-18 12:59:15 +0100

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
-var quizController = require('../controllers/quiz_controller');
+var quizController=require('../controllers/quiz_controller');
 
 //GET author
 var authorController=require('../controllers/author_controller');
-
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer',   quizController.answer);
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 //GET author
 router.get('/author/creditos', authorController.creditos);
