9b6dd3457cb69be530cfb2d9e5621b0ef128afc4
ElsaMacias <elsa.macias@gmail.com>
2015-07-27 18:36:51 +0100

    P2P obligatorio Modulo 8

--- controllers/author_controller.js
+++ controllers/author_controller.js
@@ -4,7 +4,8 @@ exports.creditos= function(req, res) {
           res.render('author/creditos',
             {
               datos:'Elsa',
-              nombre_foto:'miFoto.jpg'
+              nombre_foto:'miFoto.jpg',
+ 	      errors: []  // Esto falto ponerlo desde el commit de validar preguntas. Ahora funciona el control de errores en el body.
             }
           );
 };
