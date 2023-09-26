36458da18459f3438a6cd23885c4231ec9812f67
slugo01 <sergiocorreo24@gmail.com>
2023-09-26 12:38:59 +0100

    peticion fork

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+//holaaaa
 //Renderizar el layout.ejs y completar las vistas.
 var partials = require('express-partials');
 
